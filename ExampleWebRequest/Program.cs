a9738c70a5ae374e855fac932cba0485eaa1b97a
Eduardo Pitz <42384045+pitzeduardo@users.noreply.github.com>
2018-08-21 18:24:49 -0300

    First
    This first commit show how to obtein a JSON of VIACEP API

---
+++ ExampleWebRequest/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ExampleWebRequest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input CEP: ");
+            var cep = Console.ReadLine();
+
+            var cepService = new WebRequestService();
+            var cidadeReturn = cepService.GetCEP(cep);
+
+            Console.WriteLine(string.Format("Name of City: {0}", cidadeReturn.localidade));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ExampleWebRequest/WebRequestService.cs
@@ -0,0 +1,41 @@
+﻿using ExampleWebRequest.Models;
+using System.IO;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace ExampleWebRequest
+{
+    public class WebRequestService
+    {
+        public Cidade GetCEP(string cep)
+        {
+            var cidadeReturn = new Cidade();
+
+            WebRequest requestCEP = WebRequest.Create(ReturnURI(cep));
+            requestCEP.Credentials = CredentialCache.DefaultCredentials;
+
+            WebResponse responseCEP = requestCEP.GetResponse();
+
+            Stream responseStream = responseCEP.GetResponseStream();
+            StreamReader readerStream = new StreamReader(responseStream);
+
+            string jsonResponse = readerStream.ReadToEnd();
+            cidadeReturn = JsonConvert.DeserializeObject<Cidade>(jsonResponse);
+
+            readerStream.Close();
+            responseCEP.Close();
+
+            return cidadeReturn;
+        }
+
+        #region URI ViaCEP
+
+        private string ReturnURI(string cep)
+        {
+            return "http://www.viacep.com.br/ws/" + cep + "/json/";
+        }
+
+        #endregion 
+
+    }
+}
